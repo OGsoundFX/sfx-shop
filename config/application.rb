1db26ea5fc6ed2f2c17be5d748d16650faf8e6df
Olivier Girardot <olive_girardot@hotmail.com>
2020-10-09 16:31:40 +0200

    reverted last change

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module SfxShop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.serve_static_files = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
