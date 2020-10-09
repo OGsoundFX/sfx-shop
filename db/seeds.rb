4a9fce042e3f0e08ae4bd0fa462f2ef0b8d8c668
Olivier Girardot <olive_girardot@hotmail.com>
2020-10-09 15:16:34 +0200

    I installed active_storage and added the has_many_photos parameter to the sfx pack model. added some photos and displayed them. will now look into payment and work on separate branches from now on

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
 
 puts "destroying DB"
 
@@ -13,9 +14,11 @@ SfxPack.destroy_all
 
 designer = SoundDesigner.create(first_name: 'Olivier', last_name: 'Girardot', email: 'olive_girardot@hotmail.com', password: '123456', address: 'SonnenburegerStrasse 76, 10437 Berlin, Germany', location: 'Berlin')
 
-SfxPack.create(title:'Monster Pack', description: 'the best sounds ever', category: 'monster', tags: 'scary, monster, fantasy', size_mb: 1076, duration: '90mn', number_of_tracks: 1018, price: 20.00, sound_designer_id: designer.id)
-SfxPack.create(title:'Magic Spells Pack', description: 'the best sounds ever', category: 'spells', tags: 'scary, magic, fantasy', size_mb: 1076, duration: '80mn', number_of_tracks: 250, price: 15.00, sound_designer_id: designer.id)
+monster_pack = SfxPack.create(title:'Monster Pack', description: 'the best sounds ever', category: 'monster', tags: 'scary, monster, fantasy', size_mb: 1076, duration: '90mn', number_of_tracks: 1018, price: 20.00, sound_designer_id: designer.id)
+guns_explosions = SfxPack.create(title:'Guns and Explosions', description: 'the best sounds ever', category: 'weapons', tags: 'war, realistic, battle', size_mb: 570, duration: '80mn', number_of_tracks: 250, price: 15.00, sound_designer_id: designer.id)
 
+monster_pack.photos.attach(io: File.open(Rails.root.join("app", "assets", "images", "Monsters-pack_New-2020.jpg")), filename: 'image')
+guns_explosions.photos.attach(io: File.open(Rails.root.join("app", "assets", "images", "Guns-Explosions-Cover_New-1.jpg")), filename: 'image')
 
 puts "created #{SoundDesigner.count} sound designers"
 puts "created #{SfxPack.count} sound effect packs"
