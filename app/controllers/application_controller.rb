ad34786fca720dbc1a4ca585e0cef53875d51a7e
Olivier Girardot <olive_girardot@hotmail.com>
2020-10-19 11:18:26 +0200

    actually no coloudinary yet. Resolved update problem and attempting file protection. Testing in production now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :username, :location) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :username, :location) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :username, :location, :current_password) }
   end
 end
