9cd19c6a94c07bdd66edf153d5c1a902ab22fa24
Olivier Girardot <olive_girardot@hotmail.com>
2020-11-01 16:06:19 +0100

    re-organized sfx table and seeded the 5 packs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,25 @@
-# require "open-uri"
+require "open-uri"
 
-# puts "destroying DB"
+puts "destroying DB"
 
-# SoundDesigner.destroy_all
-# Order.destroy_all
-# Cart.destroy_all
-# SfxPack.destroy_all
+SoundDesigner.destroy_all
+Order.destroy_all
+Cart.destroy_all
+SfxPack.destroy_all
 
-# designer = SoundDesigner.create(first_name: 'Olivier', last_name: 'Girardot', email: 'olive_girardot@hotmail.com', password: '123456', address: 'SonnenburegerStrasse 76, 10437 Berlin, Germany', location: 'Berlin')
+designer = SoundDesigner.create(first_name: 'Olivier', last_name: 'Girardot', address: 'SonnenburegerStrasse 76, 10437 Berlin, Germany', location: 'Berlin', bio: 'nothing special right now')
 
-# monster_pack = SfxPack.create(sku: "OGPACK013", title:'Monster Pack', description: 'the best sounds ever', category: 'monster', tags: 'scary, monster, fantasy', size_mb: 1076, duration: '90mn', number_of_tracks: 1018, price_cents: 2000, sound_designer_id: designer.id)
-# guns_explosions = SfxPack.create(sku: "OGPACK002", title:'Guns and Explosions', description: 'the best sounds ever', category: 'weapons', tags: 'war, realistic, battle', size_mb: 570, duration: '80mn', number_of_tracks: 250, price_cents: 1550, sound_designer_id: designer.id)
-# test_pack = SfxPack.create(sku: "OGFS001", title:'Test Pack', description: 'the best sounds ever', category: 'test', tags: 'test, free', size_mb: 570, duration: '80mn', number_of_tracks: 250, price_cents: 100, sound_designer_id: designer.id)
-# test_pack2 = SfxPack.create(sku: "OGFS001", title:'Test Pack', description: 'the best sounds ever', category: 'test', tags: 'test, free', size_mb: 570, duration: '80mn', number_of_tracks: 250, price_cents: 300, sound_designer_id: designer.id)
+monster_pack = SfxPack.create(sku: "OGPACK013", title:'Monster Sounds and Atmospheres', description: 'the best sounds ever', category: 'monster, fantasy', tags: 'scary, monster, fantasy, action, adventure', size_mb: 1400, duration: '88mn', number_of_tracks: 1108, price_cents: 2500, sound_designer_id: designer.id, link: "https://www.youtube.com/embed/ymMho53LEVg", list: "https://www.ogsoundfx.com/Documents/Monster_Pack_List_of_Sounds_Unity_2020.pdf", version: 2)
+guns_explosions = SfxPack.create(sku: "OGPACK002", title:'Guns and Explosions', description: 'the best sounds ever', category: 'weapons, war', tags: 'war, realistic, battle, guns, explosions', size_mb: 500, duration: '29mn', number_of_tracks: 418, price_cents: 1900, sound_designer_id: designer.id, link: "https://soundcloud.com/ogsoundfx/guns-explosions-preview-track-2", list: "https://www.ogsoundfx.com/Documents/Guns_&_Explosions_List_of_Sounds.pdf", version: 1)
+outdoor_atmospheres = SfxPack.create(sku: "OGPACK014", title:'Outdoor Atmospheres', description: 'the best sounds ever', category: 'atmospheres', tags: 'nature, realistic, outdoor', size_mb: 5840, duration: '6 hours', number_of_tracks: 96, price_cents: 1900, sound_designer_id: designer.id, link:"https://youtu.be/75DSTMFMejg", list: "https://www.ogsoundfx.com/Documents/Outdoor_Atmospheres_List_of_Sounds.pdf", version: 1)
+underground_atmospheres = SfxPack.create(sku: "OGPACK004", title:'Underground Atmospheres', description: 'the best sounds ever', category: 'Atmospheres', tags: 'fantasy, underground, nature, cavern, atmospheres', size_mb: 1400, duration: '90mn', number_of_tracks: 32, price_cents: 1500, sound_designer_id: designer.id, link: "https://youtu.be/YhQ_E7aznQ8", list: "https://www.ogsoundfx.com/Documents/Underground_Atmospheres_List_of_SFX.pdf", version: 1)
+natural_disasters = SfxPack.create(sku: "OGPACK009", title:'Natural Disasters', description: 'the best sounds ever', category: 'Natural Occurences', tags: 'nature, atmospheres, disaster, catastrophe, volcano, eruption, explosion, earthquake, landslide, avalanche', size_mb: 840, duration: '50mn', number_of_tracks: 53, price_cents: 1500, sound_designer_id: designer.id, link: "https://youtu.be/3-vACKDRc3A", list: "https://www.ogsoundfx.com/Documents/Natural_Disasters_List_of_SFX.pdf", version: 1)
 
-# monster_pack.photos.attach(io: URI.open('https://www.ogsoundfx.com/NeWsfXsHoP/Monsters-pack_New-2020.jpg'), filename: 'image')
-# guns_explosions.photos.attach(io: URI.open('https://www.ogsoundfx.com/NeWsfXsHoP/Guns-Explosions-Cover_New-1.jpg'), filename: 'image')
-# guns_explosions.photos.attach(io: URI.open('https://res.cloudinary.com/dk9a86uhu/image/upload/v1603104054/sfx_shop/monster_zzsdcm.jpg'), filename: 'image')
-# test_pack.photos.attach(io: File.open(Rails.root.join("app", "assets", "images", "monster.jpg")), filename: 'image.jpg')
-# test_pack2.photos.attach(io: File.open(Rails.root.join("app", "assets", "images", "monster.jpg")), filename: 'image.jpg')
+monster_pack.photos.attach(io: URI.open('https://res.cloudinary.com/dk9a86uhu/image/upload/v1604053462/sfx_shop/MonsterPack_Transp_small_jhfded.png'), filename: 'image', content_type: "image/jpg")
+guns_explosions.photos.attach(io: URI.open('https://res.cloudinary.com/dk9a86uhu/image/upload/v1604052938/sfx_shop/Guns_Explosions_transp_small_rywioe.png'), filename: 'image', content_type: "image/jpg")
+outdoor_atmospheres.photos.attach(io: URI.open('https://res.cloudinary.com/dk9a86uhu/image/upload/v1604054507/sfx_shop/UndergroundAtmopsheres_transp_small_kpso85.png'), filename: 'image', content_type: "image/jpg")
+underground_atmospheres.photos.attach(io: URI.open('https://res.cloudinary.com/dk9a86uhu/image/upload/v1604053462/sfx_shop/MonsterPack_Transp_small_jhfded.png'), filename: 'image', content_type: "image/jpg")
+natural_disasters.photos.attach(io: URI.open('https://res.cloudinary.com/dk9a86uhu/image/upload/v1604054506/sfx_shop/NaturalDisasters_transp_small_uokbyk.png'), filename: 'image', content_type: "image/jpg")
 
-# monster_pack.photos.attach(io: File.open(Rails.root.join("app", "assets", "images", "test.jpg")), filename: 'image.jpg')
-
-# puts "created #{SoundDesigner.count} sound designers"
-# puts "created #{SfxPack.count} sound effect packs"
-
-# monster_pack = SfxPack.where(title: "Monster Pack")[0]
-# monster_pack.photos.attach(io: URI.open('https://www.ogsoundfx.com/NeWsfXsHoP/Monsters-pack_New-2020.jpg'), filename: 'image')
-# monster_pack.photos.attach(io: URI.open('https://res.cloudinary.com/dk9a86uhu/image/upload/v1603886106/sumqq9z2xodfcebgx160x7pll08u.jpg'), filename: 'image', content_type: "image/jpg")
-# monster_pack.photos.attach(io: File.open(Rails.root.join("app", "assets", "images", "monster.jpg")), filename: 'image.jpg')
-
-monster_pack = SfxPack.where(title: "Monster Pack")[0]
-monster_pack.link = "https://www.youtube.com/embed/ymMho53LEVg"
-monster_pack.save
+puts "created #{SoundDesigner.count} sound designers"
+puts "created #{SfxPack.count} sound effect packs"
