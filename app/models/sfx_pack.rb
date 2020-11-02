43a584955e3f3724bbae78d500ab004f09694807
Olivier Girardot <olive_girardot@hotmail.com>
2020-11-02 21:16:38 +0100

    created sfx cards for front page

--- app/models/sfx_pack.rb
+++ app/models/sfx_pack.rb
@@ -1,5 +1,6 @@
 class SfxPack < ApplicationRecord
   belongs_to :sound_designer
+  has_many :orders, dependent: :destroy
   has_many_attached :photos, dependent: :destroy
   monetize :price_cents
 end
