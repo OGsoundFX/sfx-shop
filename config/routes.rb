800900b839e80bf7ce02ebd4d13b4e9af5b7772d
Olivier Girardot <olive_girardot@hotmail.com>
2020-11-26 09:36:46 +0100

    trying to implement the reset password action in devise 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  default_url_options :host => "https://www.bamsfx.com"
+  # default_url_options :host => "https://www.bamsfx.com"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
   get "dashboard", to: "dashboards#dashboard"
