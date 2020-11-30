21f7c30b48bccb38595a1e8e928fa95d03cf11f0
Olivier Girardot <olive_girardot@hotmail.com>
2020-11-30 16:32:42 +0100

    trying lost email13

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,30 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'olive_girardot@hotmail.com'
   layout 'mailer'
+
+  # frozen_string_literal: true
+  include Devise::Mailers::Helpers
+
+  def confirmation_instructions(record, token, opts = {})
+    @token = token
+    devise_mail(record, :confirmation_instructions, opts)
+  end
+
+  def reset_password_instructions(record, token, opts = {})
+    @token = token
+    devise_mail(record, :reset_password_instructions, opts)
+  end
+
+  def unlock_instructions(record, token, opts = {})
+    @token = token
+    devise_mail(record, :unlock_instructions, opts)
+  end
+
+  def email_changed(record, opts = {})
+    devise_mail(record, :email_changed, opts)
+  end
+
+  def password_change(record, opts = {})
+    devise_mail(record, :password_change, opts)
+  end
 end
