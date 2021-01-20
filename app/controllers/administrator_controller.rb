1bd2047420b2e1220fafa866ca988a4b872c9397
Olivier Girardot <ogmusik@gmail.com>
2021-01-20 21:46:17 +0100

    implemented the create a sale action - need to apply to the packs price

--- app/controllers/administrator_controller.rb
+++ app/controllers/administrator_controller.rb
@@ -1,5 +1,6 @@
 class AdministratorController < ApplicationController
   def admin
     redirect_to root_path if current_user.nil? || current_user.email != 'olivier@ogsoundfx.com'
+    @sale = Sale.last
   end
 end
--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,11 +1,30 @@
 class SalesController < ApplicationController
   def new
     @sale = Sale.new
+
+    @packs = ["All"]
+    SfxPack.all.each do |pack|
+      @packs << pack.title
+    end
   end
 
   def create
     Sale.delete_all if Sale.count > 0
     @sale = Sale.new(sale_params)
+
+    if pack_params.include? "All"
+      pack_array = []
+      SfxPack.all.each do |pack|
+        pack_array << pack.id
+      end
+    else
+      pack_array = []
+      pack_params.each do |pack|
+        pack_array << SfxPack.where(title: pack).first.id if pack != ""
+      end
+    end
+
+    @sale.packs = pack_array
     @sale.save
     redirect_to admin_path
   end
@@ -14,5 +33,10 @@ class SalesController < ApplicationController
 
   def sale_params
     params.require(:sale).permit(:title, :percentage, :start_date, :end_date, :packs)
+  end
+
+  def pack_params
+    params.require(:sale).permit(:title, :percentage, :start_date, :end_date, :packs)
+    params[:sale][:packs]
   end
 end
