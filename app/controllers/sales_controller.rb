31a777a6a2d826ca83a9004ae4c64a08c3b2bbc0
Olivier Girardot <ogmusik@gmail.com>
2021-01-20 17:48:16 +0100

    preparing sales2

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -7,10 +7,12 @@ class SalesController < ApplicationController
     Sale.delete_all if Sale.count > 0
     @sale = Sale.new(sale_params)
     @sale.save
+    redirect_to admin_path
   end
 
   private
 
   def sale_params
+    params.require(:sale).permit(:title, :percentage, :start_date, :end_date, :packs)
   end
 end
