784a08b9ba6b451b4b551c3f3327805c68328767
Olivier Girardot <ogmusik@gmail.com>
2021-01-21 13:24:45 +0100

    changed the concept of having only one sale at a time, to multiple sales and also keeping archived sales

--- app/controllers/administrator_controller.rb
+++ app/controllers/administrator_controller.rb
@@ -1,6 +1,8 @@
 class AdministratorController < ApplicationController
   def admin
     redirect_to root_path if current_user.nil? || current_user.email != 'olivier@ogsoundfx.com'
+    @all_sales = Sale.all
+    @current_sales = Sale.where("end_date > ?", Date.current)
     @sale = Sale.last
   end
 end
