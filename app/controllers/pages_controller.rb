9d177466c1d66d778ab7d0701851b7731938b521
Olivier Girardot <ogmusik@gmail.com>
2021-01-22 14:48:25 +0100

    discount display phase 2: star icons on every discounted pack

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,14 @@ class PagesController < ApplicationController
   def home
     @designers = SoundDesigner.all
     @sfx_packs = SfxPack.all
+
+    current_sales = Sale.where("end_date > ?", Date.current)
+    @current_sales_list = {}
+    current_sales.each do |sale|
+      sale.packs.each do |pack_id|
+        @current_sales_list[pack_id] = sale.percentage
+      end
+    end
   end
 
   def about
