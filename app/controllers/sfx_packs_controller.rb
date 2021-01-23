83118b1186472bb5024c506f35905bb814d8b455
Olivier Girardot <ogmusik@gmail.com>
2021-01-23 16:07:30 +0100

    added sale price on show pages

--- app/controllers/sfx_packs_controller.rb
+++ app/controllers/sfx_packs_controller.rb
@@ -4,5 +4,11 @@ class SfxPacksController < ApplicationController
     @pack = SfxPack.find(params[:id])
     @designer = SoundDesigner.find(@pack.sound_designer_id)
     @designer_name = "#{@designer.first_name} #{@designer.last_name}"
+    current_sales = Sale.where("end_date > ?", Date.current)
+    current_sales.each do |sale|
+      sale.packs.each do |pack_id|
+        @discount = sale.percentage if @pack.id == pack_id
+      end
+    end
   end
 end
