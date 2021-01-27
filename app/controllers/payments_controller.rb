74f00350fade011f16271a0ef841018399a5c92a
Olivier Girardot <ogmusik@gmail.com>
2021-01-27 22:30:15 +0100

    updated the new payment display with sale prices

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,6 +3,18 @@ class PaymentsController < ApplicationController
     @order = current_user.orders.where(status: 'pending').find(params[:order_id])
     if @order.multiple == false
       @photo = @order.sfx_pack.photos[0]
+      items = []
+      items << @order.sfx_pack_id
+    else
+      items = @order.packs
+    end
+
+    current_sales = Sale.where("end_date > ?", Date.current)
+    @current_sales_list = {}
+    items.each do |item|
+      current_sales.each do |sale|
+        @current_sales_list[item] = sale.percentage if sale.packs.include? item
+      end
     end
   end
 end
