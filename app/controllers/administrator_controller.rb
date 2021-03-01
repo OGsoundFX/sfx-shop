c6f97a865a8703894b8ff0ddb1040798a54c34e5
olivier girardot <olive_girardot@hotmail.com>
2021-03-01 16:07:53 +0100

    started with display of packs sold in admin

--- app/controllers/administrator_controller.rb
+++ app/controllers/administrator_controller.rb
@@ -4,5 +4,24 @@ class AdministratorController < ApplicationController
     @all_sales = Sale.all
     @current_sales = Sale.where("end_date > ?", Date.current)
     @sale = Sale.last
+
+    @packs = {
+      "Monster SFX Pack" => 0,
+      "Guns and Explosions"=> 0,
+      "Outdoor Atmospheres"=> 0,
+      "Underground Atmospheres"=> 0,
+      "Natural Disasters"=> 0,
+      "Ghost & Haunted"=> 0,
+      "Weather Effects"=> 0
+    }
+    Order.all.each do |order|
+      if order.multiple
+        order.packs.each do |pack|
+          @packs[SfxPack.find(pack).title] += 1
+        end
+      else
+        @packs[SfxPack.find(order.sfx_pack_id).title] += 1
+      end
+    end
   end
 end
