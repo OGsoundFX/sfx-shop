63077ac3fea2967056bcd8cb45e03e7b299411eb
Olivier Girardot <olive_girardot@hotmail.com>
2021-09-23 09:56:07 +0200

    trying to remove price to replace it with monetized price on heroku

--- db/migrate/20210923074007_add_monetize.rb
+++ db/migrate/20210923074007_add_monetize.rb
@@ -1,5 +1,6 @@
 class AddMonetize < ActiveRecord::Migration[6.0]
   def change
+    remove_column :single_tracks, :price, :decimal
     add_monetize :single_tracks, :price, currency: { present: false }
   end
 end
