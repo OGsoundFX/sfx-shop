7a494235793d646057b7443f781a8d24d3e8462f
Olivier Girardot <olive_girardot@hotmail.com>
2021-09-23 10:06:06 +0200

    reverse change

--- db/migrate/20210923074007_add_monetize.rb
+++ db/migrate/20210923074007_add_monetize.rb
@@ -1,6 +1,5 @@
 class AddMonetize < ActiveRecord::Migration[6.0]
   def change
-    remove_column :single_tracks, :price, :decimal
     add_monetize :single_tracks, :price, currency: { present: false }
   end
 end
