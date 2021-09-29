0152c836f0f5d0e77f41826b03e47e7f640f6697
Olivier Girardot <olive_girardot@hotmail.com>
2021-09-29 15:03:18 +0200

    designed the track component for single tracks

--- app/controllers/single_tracks_controller.rb
+++ app/controllers/single_tracks_controller.rb
@@ -1,6 +1,17 @@
 class SingleTracksController < ApplicationController
   def index
-    @track = SingleTrack.first
+    @icons = {
+      "action": '<i class="fas fa-swords"></i>',
+      "medieval": '<i class="fab fa-fort-awesome"></i>',
+      "outdoor":	'<i class="fas fa-trees"></i>',
+      "underground": '<i class="fas fa-dungeon"></i>',
+      "scary":	'<i class="fas fa-ghost"></i>',
+      "monsters":	'<i class="fas fa-dragon"></i>',
+      "disasters": '<i class="fas fa-volcano"></i>',
+      "weather": '<i class="fas fa-cloud-showers-heavy"></i>',
+      "random": '<i class="fas fa-volume"></i>'
+    }
+
     @tracks = SingleTrack.all
   end
 end
