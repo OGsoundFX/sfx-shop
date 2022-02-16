99fcfc6ea783df4883417adf8e80104cb6b843dc
Olivier Girardot <olive_girardot@hotmail.com>
2022-02-16 16:34:42 +0100

    changed column price_range to points in colection_categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_02_133504) do
+ActiveRecord::Schema.define(version: 2022_02_16_153126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,10 +56,10 @@ ActiveRecord::Schema.define(version: 2022_01_02_133504) do
   end
 
   create_table "collection_categories", force: :cascade do |t|
-    t.integer "price_range", default: [], array: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price_cents", default: 0, null: false
+    t.integer "points"
   end
 
   create_table "collections", force: :cascade do |t|
