3e892dc3567366b6400c16f78cca7a5789cad519
Olivier Girardot <olive_girardot@hotmail.com>
2022-04-04 12:22:44 +0200

    fixed cart display issue

--- app/javascript/packs/wavesurfer.js
+++ app/javascript/packs/wavesurfer.js
@@ -14,8 +14,15 @@ const timeConverter = (time) => {
 
 window.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
+    // for loading the waveforms of single tracks display section in cart
     document.getElementById("single-display").classList.add('d-none')
     document.getElementById("single-display").style.visibility = ""
+    document.getElementById("wrapped-single-display").classList.remove('d-none')
+
+    // for loading the waveforms of collections display section in cart
+    document.getElementById("collection-display").classList.add('d-none')
+    document.getElementById("collection-display").style.visibility = ""
+    document.getElementById("wrapped-collection-display").classList.remove('d-none')
   }, 700);
 
 
