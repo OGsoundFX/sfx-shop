df8b35e6622de65397eac0064f09b76e0b74c60e
Olivier Girardot <olive_girardot@hotmail.com>
2022-07-01 16:18:59 +0200

    finished checkout FE

--- app/models/single_track.rb
+++ app/models/single_track.rb
@@ -8,4 +8,19 @@ class SingleTrack < ApplicationRecord
     using: {
       tsearch: { prefix: true }
     }
+
+  def self.categories
+    {
+      all: '<i class="fas fa-volume-up"></i>',
+      action: '<i class="fas fa-swords"></i>',
+      medieval: '<i class="fab fa-fort-awesome"></i>',
+      outdoor:	'<i class="fas fa-trees"></i>',
+      underground: '<i class="fas fa-dungeon"></i>',
+      scary:	'<i class="fas fa-ghost"></i>',
+      monsters:	'<i class="fas fa-dragon"></i>',
+      disasters: '<i class="fas fa-volcano"></i>',
+      weather: '<i class="fas fa-cloud-showers-heavy"></i>',
+      default: '<i class="fas fa-volume-up"></i>'
+    }
+  end
 end
