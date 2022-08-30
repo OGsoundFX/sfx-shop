213685c95024fff7a57b07cfda4861a556705038
Olivier Girardot <olive_girardot@hotmail.com>
2022-08-30 12:50:11 +0200

    added a announcement to single tracks plus updraged pg gem

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'dotenv-rails', groups: [:development, :test]
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 # Use postgresql as the database for Active Record
-gem 'pg', '>= 0.18', '< 2.0'
+gem 'pg', '~> 1.4', '>= 1.4.3'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_04_142202) do
+ActiveRecord::Schema.define(version: 2022_07_19_161511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(version: 2022_04_04_142202) do
     t.integer "sample_rate"
     t.integer "batch"
     t.string "preview_link"
+    t.integer "announcement"
     t.index ["sound_designer_id"], name: "index_single_tracks_on_sound_designer_id"
   end
 
