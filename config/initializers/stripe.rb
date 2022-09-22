d69665a0e4540f91feeea706025f8a39d6e7b939
Olivier Girardot <olive_girardot@hotmail.com>
2022-09-22 18:43:43 +0200

    adding tax rate in stripe checkout config

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -8,7 +8,7 @@ Rails.configuration.stripe = {
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
-Stripe.tax_rate = Rails.configuration.stripe[:tax_rate]
+StripeEvent.tax_rates = Rails.configuration.stripe[:tax_rate]
 
 StripeEvent.configure do |events|
   events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
