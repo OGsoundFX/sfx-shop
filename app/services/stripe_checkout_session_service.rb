e221b8ccfc502843ef75324e17f7a2acd2b5bad6
Olivier Girardot <olive_girardot@hotmail.com>
2022-09-22 18:55:52 +0200

    solving checkout 1

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,5 +1,6 @@
 class StripeCheckoutSessionService
   def call(event)
+    raise
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(status: 'paid')
     order.update(amount_paid_cents: event.data.object.amount_total)
