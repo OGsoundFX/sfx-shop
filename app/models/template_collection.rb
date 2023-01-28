74f4dfc72d033cf016ac400a2a0b83e2f70d94c1
Olivier Girardot <olive_girardot@hotmail.com>
2023-01-28 16:09:32 +0100

    template collection has one attached photo

--- app/models/template_collection.rb
+++ app/models/template_collection.rb
@@ -1,6 +1,7 @@
 class TemplateCollection < ApplicationRecord
   before_validation :order_tracks
   monetize :price_cents
+  has_one_attached :photo
 
   validates :title, presence: true, uniqueness: true
   validates :tracks, uniqueness: true
