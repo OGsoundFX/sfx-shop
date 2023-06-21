c9253623845c809dbc013d3de559011ab9501df4
Olivier Girardot <olive_girardot@hotmail.com>
2023-06-21 22:49:17 +0200

    fixed the template creation process and replaced the attached photo to appropriate category

--- app/models/template_collection.rb
+++ app/models/template_collection.rb
@@ -1,7 +1,6 @@
 class TemplateCollection < ApplicationRecord
   before_validation :order_tracks
   monetize :price_cents
-  has_one_attached :photo
 
   validates :title, presence: true, uniqueness: true
   validates :tracks, uniqueness: true
