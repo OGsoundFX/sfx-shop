2e34806ccfff76e34f5f92c300d267c7c6bd1c7a
Olivier Girardot <olive_girardot@hotmail.com>
2023-06-22 14:43:23 +0200

    adjusted FE issues on template index

--- app/javascript/packs/modals.js
+++ app/javascript/packs/modals.js
@@ -5,15 +5,17 @@ window.addEventListener('DOMContentLoaded', () => {
   const modal = document.getElementById("learnMore");
   
   // Get the button that opens the modal
-  const btn = document.getElementById("learnMoreBtn");
+  const btns = document.querySelectorAll(".learnMoreBtn");
   
   // Get the <span> element that closes the modal
   const span = document.getElementsByClassName("learn-more-close")[0];
   
-  // When the user clicks the button, open the modal 
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
+  // When the user clicks the button, open the modal
+  btns.forEach(btn => {
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+  })
   
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
