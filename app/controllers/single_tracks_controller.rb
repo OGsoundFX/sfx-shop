cd20ed4d13362effa064d1e3a324d7022cb41679
Olivier Girardot <olive_girardot@hotmail.com>
2023-07-05 15:02:06 +0200

    not sure of improvements made here

--- app/controllers/single_tracks_controller.rb
+++ app/controllers/single_tracks_controller.rb
@@ -22,7 +22,7 @@ class SingleTracksController < ApplicationController
     @newest = true if params[:order_by_dropdown] == "newest"
 
     # order by
-    if params[:order_by_dropdown] != nil && params[:order_by_dropdown] != "" || params[:dropdown] == "all"
+    if params[:order_by_dropdown] != nil && params[:order_by_dropdown] != ""
       if params[:previous_category] != ""
         if params[:order_by_dropdown] == "newest"
           @tracks = SingleTrack.where(category: params[:previous_category]).reorder(created_at: :desc).page params[:page]
@@ -99,6 +99,7 @@ class SingleTracksController < ApplicationController
         end
       end
     end
+
     # filters
     if params[:filters] != nil && params[:filters] != ""
       points_grid = SingleTrack.points_grid
@@ -155,7 +156,7 @@ class SingleTracksController < ApplicationController
         @tracks = SingleTrack.where("size < #{filters[:maxsize]} and size > #{filters[:minsize]} and points < #{filters[:maxpoints]} and points >= #{filters[:minpoints]} #{'and loop = true' if loopable == true} #{'and fantasy = true' if fantasy == true} #{'and fantasy = false' if fantasy == false}").page params[:page]
       end
     elsif params[:filters] == ""
-      if params[:dropdown] && params[:dropdown] != "" && params[:dropdown] != "all"
+      if params[:dropdown] && params[:dropdown] != ""
         @tracks = SingleTrack.where(category: params[:dropdown]).page params[:page]
         @dropdown = params[:dropdown]
         @search = "Search by keyword"
