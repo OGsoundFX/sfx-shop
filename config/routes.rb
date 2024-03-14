64984fc13b34365ff52af487a58f717f21be3623
Olivier Girardot <olive_girardot@hotmail.com>
2024-03-14 11:25:00 +0100

    added rails admin path in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin2', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/rails-admin', as: 'rails_admin'
   get 'collections/create'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
