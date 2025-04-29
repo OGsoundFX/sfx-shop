7098d35234d438bb3a41f4bb9a135a14a9bf63d4
Olivier Girardot <olive_girardot@hotmail.com>
2025-04-29 17:20:32 +0200

    added the active boolean column to the template collection table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_06_201130) do
+ActiveRecord::Schema.define(version: 2025_04_29_151757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -178,6 +178,7 @@ ActiveRecord::Schema.define(version: 2024_03_06_201130) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price_cents", default: 0, null: false
+    t.boolean "active", default: true
   end
 
   create_table "users", force: :cascade do |t|
