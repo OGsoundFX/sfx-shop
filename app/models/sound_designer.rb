5247492d0aeff6ed532325559396b1f2cc1907fb
Olivier Girardot <olive_girardot@hotmail.com>
2025-06-15 17:23:51 +0200

    fixed image display when designer has no image

--- app/models/sound_designer.rb
+++ app/models/sound_designer.rb
@@ -3,5 +3,11 @@ class SoundDesigner < ApplicationRecord
   has_many :sfx_packs, dependent: :destroy
   has_many :single_tracks, dependent: :destroy
 
+  validates :first_name, :last_name, presence: true
+
   has_one_attached :photo
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
 end
