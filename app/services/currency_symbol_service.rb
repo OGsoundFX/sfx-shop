15c60f22657f294e100000367087b26ea8a1fe0e
Olivier Girardot <olive_girardot@hotmail.com>
2025-09-10 09:32:02 +0200

    quick fix to home page crashing because of currency_rate process not working

--- app/services/currency_symbol_service.rb
+++ app/services/currency_symbol_service.rb
@@ -7,4 +7,17 @@ module CurrencySymbolService
       "usd" => "$"
     }
   end
+
+  # possibly translate to the following (constant + lookup method)
+
+  #   MAP = {
+  #   "$"  => "usd",
+  #   "€"  => "eur",
+  #   "usd" => "$",
+  #   "eur" => "€"
+  # }.freeze
+
+  # def self.lookup(value)
+  #   MAP[value]
+  # end
 end
