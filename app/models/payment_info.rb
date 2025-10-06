8a4e72121ec0c656852685d3d8ef59113095b3be
Olivier Girardot <olive_girardot@hotmail.com>
2025-10-06 14:58:08 +0200

    preferred currency defines the currency of the pack the sound designer sell. Once established it can only be changed by request directly to Bamsfx

--- app/models/payment_info.rb
+++ app/models/payment_info.rb
@@ -3,4 +3,5 @@ class PaymentInfo < ApplicationRecord
 
   enum status: {pending_verif: 0, active: 1, inactive: 2}
   enum preferred_method: {paypal: 0, bank_transfer: 1}
+  enum preferred_currency: {usd: 0, eur: 1}
 end
