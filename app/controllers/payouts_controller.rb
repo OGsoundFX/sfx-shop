7f4be8423ffe902a3da4a81461f3ccd3ec3f5904
Olivier Girardot <olive_girardot@hotmail.com>
2025-12-05 13:53:48 +0100

    implemented admin payout feature

--- app/controllers/payouts_controller.rb
+++ app/controllers/payouts_controller.rb
@@ -3,10 +3,31 @@ class PayoutsController < ApplicationController
     @designer = SoundDesigner.find(params[:payout][:designer])
     @payout_info = params[:payout][:paypal_account]
     @sold_items = params[:sold_items].map { |item| GlobalID.find(item)}
+    @payout = Payout.new
   end
 
   def create
-    raise
+    designer = SoundDesigner.find(params[:payout][:designer_id])
+    currency = designer.payment_infos.last.preferred_currency
+    paypal_account = designer.payment_infos.last.paypal_account
+    sold_items = SoldItem.where(id: params[:sold_item_ids])
+    amount_paid = sold_items.sum(&:payout_amount_cents)
+
+    payout = Payout.create(
+      sound_designer: designer,
+      status: "paid",
+      payout_date: Date.today,
+      currency: currency,
+      amount_cents: amount_paid,
+      paypal_account: paypal_account
+    )
+    sold_items.each(&:paid!)
+    sold_items.each do |item|
+      item.payout = payout
+      item.save
+    end
+
+    redirect_to admin_payouts_path
   end
 
   private
