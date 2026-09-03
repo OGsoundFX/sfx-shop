098274b7b3e8538759aaf5478461b3b4df975575
Olivier Girardot <olive_girardot@hotmail.com>
2026-09-03 17:09:05 +0200

    implemented mailer to admin for pack submission

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,4 +4,10 @@ class AdminMailer < ApplicationMailer
     @designer_submission = submission
     mail(to: emails, subject: "New seller submission to BamSFX!")
   end
+
+  def new_pack_submission(pack)
+    emails = User.where(admin: true).pluck(:email)
+    @pack_submission = pack
+    mail(to: emails, subject: "New pack submission to BamSFX!")
+  end
 end
